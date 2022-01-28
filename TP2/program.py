b162e9f96f7e5adb2de2317393f95cfb6a711467
El Ansari Fayssal <elansari@a12p9.fil.univ-lille1.fr>
2022-01-28 14:24:33 +0100

    2eme partie tp

--- TP2/program.py
+++ TP2/program.py
@@ -7,10 +7,22 @@ genebank = SeqIO.read("data/NM_007389.gb", "genbank")
 
 # 2.comparaison entre les 2 sequence lus
 if (str(fasta.seq) == str(genebank.seq)): # True
-    print("les 2 sequences sont identiques")
+    print("2. les 2 sequences sont identiques")
 
 # 3.passage au complement inverse des sequences
 fasta.reverse_complement(id = fasta.id + "_rc")
 genebank.reverse_complement(id = genebank.id + "_rc")
 
 # 4.
+print("4.a la longeur du format fasta est: " + str(len(fasta.features)))
+print("    la longeur du format genebank est: " + str(len(genebank.features)))
+# le format fasta n'a pas de features (la longeur de sa liste features est 0)
+print("4.b les features du format genebank sont: ")
+for feature in genebank.features:
+    print("  " +feature.type)
+
+# 5. position de debut et de fin de la premiere feature
+print("debut: " + str(genebank.features[0].location.start))
+print("fin: " + str(genebank.features[0].location.end))
+
+
---
+++ TP2/src/utils.py
@@ -0,0 +1,5 @@
+from Bio import SeqIO
+
+def find_cds(seqRecord):
+    for 
+
