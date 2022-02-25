a89a489d2dc96f64cf080fedc489ddb9279af5c3
El Ansari Fayssal <elansari@a12p23.fil.univ-lille1.fr>
2022-02-25 16:18:21 +0100

    m

--- TP3/scan_pwm.py
+++ TP3/scan_pwm.py
@@ -1,10 +1,31 @@
 from pwm import *
 from utils import *
+import sys
 
-def main():
-    pass
+def main(args):
+    if (len(args) < 4): 
+        print("please add params!")
+        exit()
+    # fichier contenant une matrice JASPAR (dirrection dossier)
+    print(args[0])
+    fichier = args[0]
+    # un identifiant Genbank d’un mRNA ;
+    print(args[1])
+    idGenbank = args[1]
+    # une taille pour la séquence promotrice ;
+    tailleSequencePromotrice = int(args[2])
+    # un seuil de score.
+    print(args[3])
+    seuilScore = int(args[3])
+
+    pseudo_weight = 0.1
+    seq = find_cds(idGenbank)
+    pssm = pwm2pssm(matrix, seuilScore, False)
+    scan_res = scan_sequence(pssm, seq, -2)
+
+            
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
 
