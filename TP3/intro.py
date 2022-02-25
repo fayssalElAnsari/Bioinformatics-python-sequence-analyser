88787015e5ee0dc236896a147bb4b17d7ab25564
Angel <angeldaniel.pastorrojas@gmail.com>
2022-02-25 14:56:08 +0100

    .

--- TP3/intro.py
+++ TP3/intro.py
@@ -1,13 +1,12 @@
 from Bio import motifs
 
 # Calcul de score à partir de matrices de fréquences
-    # 1. Combien de matrices ont été lues ?
+    # 1. Combien de matrices ont été lues ? => 1
 with open("data/MA0037.2.jaspar") as handle:
         m = motifs.read(handle, "jaspar")
         print(m.consensus)
         print(m)
-    # 2. De quelle manière allez-vous pouvoir obtenir une matrice poids-position (position-weight matrix, PWM) ?
-        print(m.counts)
+    # 2. De quelle manière allez-vous pouvoir obtenir une matrice poids-position (position-weight matrix, 
+        print(m.counts.normalize())
             #  Quelle valeur mettez-vous pour les pseudo-poids (pseudocounts) ?
-        print(m.counts.normalize)
-        print(m.counts.normalize().log_odds)
+        
