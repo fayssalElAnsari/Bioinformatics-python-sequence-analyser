4ebe0c47152b51000f29b52bcfc5f1e9a3e6d5a2
El Ansari Fayssal <elansari@a12p23.fil.univ-lille1.fr>
2022-02-25 15:37:27 +0100

    part1

--- TP3/pwm.py
+++ TP3/pwm.py
@@ -1,5 +1,6 @@
 from Bio import SeqIO, Entrez, motifs
 from Bio.SeqFeature import FeatureLocation
+from Bio.Seq import Seq
 import json
 
 Entrez.email = "fayssal.el.ansari@gmail.com"
@@ -10,17 +11,29 @@ def jaspar2pwm():
         for m in motifs.parse(handle, "jaspar"):
             print(m)
             print("normalizing...")
-            print("PWM:")
-            print(m.counts.normalize(50)) # 50 by trial to get a 0.0x difference
+            print("PWM with pseudo wieght of 50:")
+            pwm = m.counts.normalize(50)
+            print(pwm) # 50 by trial to get a 0.0x difference
+            pwm2 = m.counts.normalize(0.01)
             print("PSSM")
-            print(m.counts.normalize(0.01).log_odds())
-
+            pssm = pwm2.log_odds() 
+            print(pssm)
+            print("PWM score for 'ACGTACGT': ")
+            calculate_res = pssm.calculate(Seq("ACGTACGT"))
+            print(calculate_res)
+            max_score = pssm.max
+            min_score = pssm.min
+            abs_score_threshold = (max_score - min_score) * 0.8 + min_score
+            print("recherche de la Seq('AGATAAGA') avec un seuil de ", abs_score_threshold , " : ")
+            for position, score in pssm.search(Seq("AGATAAGA"), threshold=abs_score_threshold):
+                print("Position %d: score = %5.3f" % (position, score))
             # motifs.calculate(m)
             count = count + 1
 
-    print("Le nombre des matrices lus: " + str(count))
+    # print("Le nombre des matrices lus: " + str(count))
 
 def pwm2pssm(matrix, pseudo_weight):
+
     pass
 
 
