e39693a6e1fd946b5589070b7ddd17e561680d99
El Ansari Fayssal <elansari@a12p24.fil.univ-lille1.fr>
2022-03-04 16:08:36 +0100

    checkpoint

--- src/utils.py
+++ src/utils.py
@@ -58,6 +58,29 @@ def upstream_gene_seq(pmid):
     # print(record["DocumentSummarySet"]["DocumentSummary"][0]["GenomicInfo"][0]["ChrAccVer"])
     return record["DocumentSummarySet"]["DocumentSummary"][0]["GenomicInfo"][0]["ChrAccVer"]
 
+
+def download_promotors(l_mrna, taille_seq, dir="."):
+    '''
+    cette fonctoin prend en parametre:
+    * l_mrna: une liste d'identifiants mrna,
+    * taille_seq: une taille de sequence de gene a recupere
+    * dir: le repetoire destination
+
+    Etant donné une liste d’identifiants de mRNA, une taille de séquence promotrice, 
+    un répertoire d’enregistrement (. par défaut), télécharge dans des fichiers séparés 
+    les séquences promotrices de ces mRNA au format FASTA
+
+    >>> list_mrna = ["NM_007389", "NM_079420", "NM_001267550", "NM_002470", "NM_003279", "NM_005159", 
+    >>> "NM_003281", "NM_002469", "NM_004997", "NM_004320", "NM_001100", "NM_006757"]
+    >>> download_promotors(list_mrna, 1024, "../data")
+    '''
+    for mrna in l_mrna:
+        nom_fichier = str(mrna) + "_" + str(taille_seq) + ".fa"
+        chemin_fichier = os.getcwd().chdir(nom_fichier)
+        id = Entrez.esearch(mrna)
+        sequence = Entrez.efetch(db="gb", id=id, rettype="fasta", retmode="text") 
+        print(sequence)
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
