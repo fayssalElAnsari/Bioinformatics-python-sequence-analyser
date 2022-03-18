2bd1a595429c0f2f4837917c019d410329ac0d57
fayssalElAnsari <fayssal.el.ansari@gmail.com>
2022-03-18 09:46:12 +0100

    added unittest structure
    CI

---
+++ test/test_utils.py
@@ -0,0 +1,21 @@
+import unittest
+
+class test_utils(unittest.TestCase):
+
+    def test_find_cds(self):
+        pass
+
+    def test_compare_rec_seq(self):
+        pass
+
+    def mrna_to_gene(self):
+        pass
+
+    def upstream_gene_seq(self):
+        pass
+
+    def download_promotors(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
