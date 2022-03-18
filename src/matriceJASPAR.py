2c495f08c102c3f3b3c54b95c9f9183f40ea0d70
angel.pastor <pas@d>
2022-03-18 13:50:10 +0100

    Un fichier jaspar la dernière version ou la matrice demander (base ID)

--- src/matriceJASPAR.py
+++ src/matriceJASPAR.py
@@ -3,7 +3,7 @@ import urllib.request
 import sys
 
 def matriceJaspar(matrice):
-    """Creer dans le fichier jaspar dans le repertoire "../data", la matrice passer en paramètre
+    """Creer dans le fichier jaspar dans le repertoire "../data", la matrice passer en paramètre sa dernière version ou celle demander
     matrice(str) : Matrice ID (pour une version spécifique), Base ID (pour la dèrniere version de cette matrice)
 
     Exemple:
@@ -12,7 +12,7 @@ def matriceJaspar(matrice):
     link = "https://jaspar.genereg.net/api/v1/matrix/"+matrice+".jaspar"
     req_url = urllib.request.urlopen(link)
     src=req_url.read()
-    fichier=open("../data/"+matrice+".jaspar","wb")
+    fichier=open("data/"+matrice+".jaspar","wb")
     fichier.write(src)
     fichier.close()
 
@@ -26,5 +26,5 @@ def matricesJaspar(list_matrice):
 if __name__ == "__main__":
     #sur le shell
     # Exemple:
-    # ~/bioinfo-tps-el_ansari-pastor_rojas/src$python3 matriceJASPAR.py MA0037.3 MA0001
+    # ~/bioinfo-tps-el_ansari-pastor_rojas$ python3 src/matriceJASPAR.py MA0001
     matricesJaspar(sys.argv[1:len(sys.argv)])
