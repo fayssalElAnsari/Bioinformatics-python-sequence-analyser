5694ddd9ddc694e5abf40105100426a679349af5
fayssalElAnsari <fayssal.el.ansari@gmail.com>
2022-03-18 15:05:37 +0100

    Update scan_pwm.py
    now for multiple matrices and one sequence

--- src/scan_pwm.py
+++ src/scan_pwm.py
@@ -24,10 +24,6 @@ def main(args):
     seq_start = int(record["DocumentSummarySet"]["DocumentSummary"][0]["GenomicInfo"][0]["ChrStart"]) - tailleSequencePromotrice
     seq_stop = int(record["DocumentSummarySet"]["DocumentSummary"][0]["GenomicInfo"][0]["ChrStart"]) - 1
 
-    print(accession_nb)
-    print(seq_start)
-    print(seq_stop)
-
     fasta_handle = Entrez.efetch(db="nucleotide", id=accession_nb, rettype="fasta", retmode="text", strand=1, seq_start=seq_start, seq_stop=seq_stop)
     seq = str(SeqIO.read(fasta_handle, "fasta").seq)
 
@@ -39,9 +35,8 @@ def main(args):
             matrix_id = matrix.matrix_id
             pssm = pwm2pssm(matrix, pseudo_weight, False)
             scan_res = scan_sequence(pssm, seq, seuilScore).get_result()
-    
-    for position, score in scan_res:
-        print(matrix_id + " " + str(position) + " " + str(score)) 
+            for position, score in scan_res:
+                print(matrix_id + " " + str(position) + " " + str(score)) 
 
 
 if __name__ == "__main__":
